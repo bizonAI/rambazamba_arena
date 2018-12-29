264ed0fb0abc2182b052d97dad8527d1703707f8
Olivenders <32130024+Olivenders@users.noreply.github.com>
2018-12-29 15:49:43 +0100

    Kranker Change

---
+++ Rambazamba_Arena/Assets/Scripts/BrokenBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenBox : MonoBehaviour
+{
+
+    public GameObject brokenSupplyBox;
+
+    public int HitsToDestroy = 3;
+    public int CurrentHitAmount = 0;
+
+    // Use this for initialization
+    private void Update()
+    {
+
+        if (CurrentHitAmount >= HitsToDestroy)
+        {
+            Instantiate(brokenSupplyBox, gameObject.transform.position, gameObject.transform.rotation);
+            Destroy(gameObject);
+        }
+
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Item")
+        {
+            CurrentHitAmount += 1;
+
+        }
+    }
+}
